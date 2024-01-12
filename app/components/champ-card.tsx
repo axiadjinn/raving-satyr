bd9e4231962d7e997569f8e669213491ca7d614b
axiadjinn <steam.axia@gmail.com>
2024-01-11 17:01:57 -0800

    Return a blank champion card when no ID was provided to the ChampCard component.

--- app/components/champ-card.tsx
+++ app/components/champ-card.tsx
@@ -1,19 +1,36 @@
-import { Center, Flex, Heading } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading } from "@chakra-ui/react";
 import ChampIcon from "./champ-icon";
 import champions from "../_data/champions";
 
 function ChampCard({ championId }: { championId: string }) {
   return (
     <Flex>
-      <Center pe={2}>
-        <ChampIcon championId={championId} />
-      </Center>
-      <Center>
-        <Heading size="md">
-          {championId ? champions.data[championId].name : '-'}
-        </Heading>
-      </Center>
-    </Flex>
+      {
+        championId
+          ? (
+            <>
+              < Center pe={2} >
+                <ChampIcon championId={championId} />
+              </Center >
+              <Center>
+                <Heading size="md">
+                  {championId ? champions.data[championId].name : '-'}
+                </Heading>
+              </Center>
+            </>
+          )
+          : (
+            <>
+              < Center pe={2} >
+                <Box boxSize="60px" bgColor="gray700"></Box>
+              </Center >
+              <Center>
+                <Heading size="md">-</Heading>
+              </Center>
+            </>
+          )
+      }
+    </Flex >
   );
 }
 
