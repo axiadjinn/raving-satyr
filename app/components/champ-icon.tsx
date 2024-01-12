d7f7910c82ace8ee2dc2fa761593ccea2d5b13bb
axiadjinn <steam.axia@gmail.com>
2024-01-11 16:38:40 -0800

    The the champion name as the alt value for champ icons.

--- app/components/champ-icon.tsx
+++ app/components/champ-icon.tsx
@@ -1,15 +1,17 @@
 import { Box, Image } from "@chakra-ui/react";
+import championsData from "../_data/champions";
 
-function ChampIcon({ champion }: { champion: string }) {
+function ChampIcon({ championId }: { championId: string }) {
   const version = '13.24.1'; // Make environment variable or build configs
-  const imageRoute = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion}.png`;
+  const champion = championsData.data[championId];
+  const imageRoute = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.id}.png`;
 
   return (
     <Box>
       {
         champion
         ? (
-          <Image boxSize="60px" src={imageRoute} alt='Dan Abramov' />
+          <Image boxSize="60px" src={imageRoute} alt={champion.name} />
         )
         : (
           <Box boxSize="60px" bgColor="gray700"></Box>
