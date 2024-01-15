6b9947cb9558214e9e3b9c12cf82d89a9f14bfd1
axiadjinn <steam.axia@gmail.com>
2024-01-15 13:35:40 -0800

    Create a champ selection component and use it to pick the opponent champ for a game in a split.

---
+++ app/components/champ-selection.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import ChampionsClient from "../_clients/champions-client/champions-client";
+import ChampCard from "./champ-card";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+} from "@chakra-ui/react";
+
+function ChampSelection({
+  onChampSelection,
+}: {
+  onChampSelection: (championData: ChampionData) => void;
+}) {
+  const [champions, setChampions] = useState<ChampionData[]>(
+    ChampionsClient.getChampionsArray(),
+  );
+
+  const [champSearchValue, setChampionSearchValue] = useState<string>("");
+
+  useEffect(() => {
+    setChampions(ChampionsClient.searchChampions(champSearchValue));
+  }, [champSearchValue]);
+
+  return (
+    <span>
+      <FormControl>
+        <HStack my="2">
+          <FormLabel>Champion: </FormLabel>
+          <Input
+            placeholder="Champion name"
+            value={champSearchValue}
+            onChange={(e) => setChampionSearchValue(e.target.value)}
+          ></Input>
+        </HStack>
+      </FormControl>
+      {champions.map((champion) => {
+        return (
+          <Button
+            size="xl"
+            variant="ghost"
+            colorScheme="gray"
+            p={2}
+            onClick={() => onChampSelection(champion)}
+          >
+            <Box m={2}>
+              <ChampCard championId={champion.id} key={champion.id} />
+            </Box>
+          </Button>
+        );
+      })}
+    </span>
+  );
+}
+
+export default ChampSelection;
--- app/splits/[id]/split-game-match-details-panel.tsx
+++ app/splits/[id]/split-game-match-details-panel.tsx
@@ -1,6 +1,20 @@
 import ChampCard from "@/app/components/champ-card";
+import ChampSelection from "@/app/components/champ-selection";
 import ThreeStateSelection from "@/app/components/three-state-selection";
-import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Text,
+  VStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function SplitGameMatchDetailsPanel({
   splitGame,
@@ -9,6 +23,8 @@ function SplitGameMatchDetailsPanel({
   splitGame: SplitGame;
   onUpdate: (splitGame: SplitGame) => void;
 }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   function onWinChanged(win: boolean | null) {
     onUpdate({
       ...splitGame,
@@ -16,38 +32,56 @@ function SplitGameMatchDetailsPanel({
     });
   }
 
-  function onOpponentClick() {
-    console.log("Opponent clicked");
+  function onOppositeChampionSelected(championData: ChampionData) {
+    onUpdate({
+      ...splitGame,
+      opponentChampionId: championData.id,
+    });
+
+    onClose();
   }
 
   return (
-    <VStack alignItems="flex-end">
-      <Box>
-        <Text display="inline" pe={2}>
-          Won the match?
-        </Text>
-        <ThreeStateSelection
-          selection={splitGame.win}
-          onStateChange={onWinChanged}
-        />
-      </Box>
-      <Box>
-        <Text display="inline-block" pe={2}>
-          Opponent champion:
-        </Text>
-        <Button
-          size="xl"
-          variant="outline"
-          colorScheme="red"
-          p={2}
-          onClick={onOpponentClick}
-        >
-          <Box verticalAlign="middle" display="inline-block">
-            <ChampCard championId={splitGame.opponentChampionId}></ChampCard>
-          </Box>
-        </Button>
-      </Box>
-    </VStack>
+    <>
+      <VStack alignItems="flex-end">
+        <Box>
+          <Text display="inline" pe={2}>
+            Won the match?
+          </Text>
+          <ThreeStateSelection
+            selection={splitGame.win}
+            onStateChange={onWinChanged}
+          />
+        </Box>
+        <Box>
+          <Text display="inline-block" pe={2}>
+            Opponent champion:
+          </Text>
+          <Button
+            size="xl"
+            variant="outline"
+            colorScheme="red"
+            p={2}
+            onClick={onOpen}
+          >
+            <Box verticalAlign="middle" display="inline-block">
+              <ChampCard championId={splitGame.opponentChampionId}></ChampCard>
+            </Box>
+          </Button>
+        </Box>
+      </VStack>
+      <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <ChampSelection onChampSelection={onOppositeChampionSelected} />
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
